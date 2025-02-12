24d6128ead05eec00422356c46662ee620e5ac6d
mikail-eliyah-00 <eliyah.mikail@gmail.com>
2025-02-11 16:54:30 -0800

    .

---
+++ scripts/utilities/dir_struct_make.sh
@@ -0,0 +1,17 @@
+# Load configurations
+source config.txt
+
+# Create directories
+braces="{$domains}/{$domain_subs}"
+file_out="$folder_out/$braces"
+eval mkdir -p $file_out
+
+# Create files within the defined range inside each subdirectory
+for dir in $(eval echo $file_out); do
+    #files="$dir/$file_name{$index_start..$index_end}.txt"
+    #eval touch $files
+    for i in $(seq -f "%03g" $index_start $index_end); do
+        touch "$dir/$file_name$i.txt"
+    done
+done
+
